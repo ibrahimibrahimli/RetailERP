7e0b0698e7e5ca08413fc21f17513c6287cfdb64
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2026-06-05 21:35:31 +0200

    Revert "Refactor and add employee activation/deactivation"
    This reverts commit e3097bd014a4371c029e4b83db9f7e67aa36d661.

--- Application/Features/Employees/Commands/DeactivateEmployee/DeactivateEmployeeCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using Application.Common.Results;
-using MediatR;
-
-namespace Application.Features.Employees.Commands.DeactivateEmployee
-{
-    public sealed record DeactivateEmployeeCommand(
-        Guid EmployeeId) : IRequest<Result>;
-}
